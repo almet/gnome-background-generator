563e64c2b93e5b71cd2c9639b98a8781d65fa058
Alexis Metaireau <ametaireau@gmail.com>
2010-10-28 01:27:07 +0100

    bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ if sys.version_info < (2,7):
 
 setup(
     name = "gnome-background-generator",
-    version = '1.0.4',
+    version = '1.1',
     url = 'http://github.com/ametaireau/gnome-background-generator/',
     author = 'Alexis Metaireau',
     author_email = 'alexis@notmyidea.org',
